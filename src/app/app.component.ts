721c35b6a9b5cff077a9f8ea196e00d95ea8d82b
Lucas Barrenechea <lucascode07@gmail.com>
2024-11-19 23:18:33 -0500

    first commit, maquetación pantalla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'webflow-codes-frontend';
+  private readonly _fb = inject(FormBuilder);
+
+  public showLoader = signal<boolean>(false);
+
+  public codeForm = this._fb.group({
+    code: [
+      '',
+      [Validators.required, Validators.minLength(6), Validators.maxLength(6)],
+    ],
+  });
+
+  public onSubmit() {
+    this.showLoader.set(true);
+
+    if (this.codeForm.invalid) return;
+  }
 }
